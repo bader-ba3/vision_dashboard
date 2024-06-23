cf829d52bc96a7e1eb0704791f7bfdfd1e617647
alidabol <alidabol.ae@gmail.com>
2024-06-23 16:04:39 +0400

    ali

--- lib/models/Student_Model.dart
+++ lib/models/Student_Model.dart
@@ -1,66 +1,74 @@
 import 'dart:math';
 
-
 import 'package:faker/faker.dart';
 
 import 'Employee_Model.dart';
 import 'event_record_model.dart';
 
 class StudentModel {
-  String? studentName,studentID,gender,StudentBirthDay,grade,parentId,section,startDate,stdClass;
-  String? studentNumber;
-  bool? available=true;
+  String? studentName,
+      studentID,
+      gender,
+      StudentBirthDay,
+      grade,
+      parentId,
+      section,
+      startDate,
+      stdClass;
+  String? studentNumber, stdExam;
+  bool? available = true;
 
   List<EventRecordModel>? eventRecords;
   String? bus;
+
   // String? guardian;
 
   StudentModel({
-     this.studentName,
-     this.studentNumber,
-     this.studentID,
-
-     this.gender,
-     this.StudentBirthDay,
-     this.grade,
-
-     this.startDate,
-
-     this.eventRecords,
-     this.bus,
-     this.parentId,
+    this.studentName,
+    this.studentNumber,
+    this.studentID,
+    this.stdExam,
+    this.gender,
+    this.StudentBirthDay,
+    this.grade,
+    this.startDate,
+    this.eventRecords,
+    this.bus,
+    this.parentId,
     this.available,
     this.section,
     this.stdClass,
   });
 
   Map<String, dynamic> toJson() => {
-    'studentName': studentName,
-    'studentNumber': studentNumber,
-    'studentID': studentID,
-    'gender': gender,
-    'StudentBirthDay': StudentBirthDay,
-    'stdClass': stdClass,
-    'grade': grade,
-    'section': section,
-    'startDate': startDate!,
-    'eventRecords': eventRecords!.map((event) => event.toJson()).toList(),
-    'parentId': parentId!,
-    'bus': bus,
-  };
+        'studentName': studentName,
+        'studentNumber': studentNumber,
+        'studentID': studentID,
+        'gender': gender,
+        'StudentBirthDay': StudentBirthDay,
+        'stdClass': stdClass,
+        'grade': grade,
+        'stdExam': stdExam,
+        'section': section,
+        'startDate': startDate!,
+        'eventRecords': eventRecords!.map((event) => event.toJson()).toList(),
+        'parentId': parentId!,
+        'bus': bus,
+      };
 
   factory StudentModel.fromJson(Map<String, dynamic> json) {
     return StudentModel(
       studentName: json['studentName'],
       studentNumber: json['studentNumber'],
       studentID: json['studentID'],
-      parentId:json['parentId'],
-      section:json['section'],
-      stdClass:json['stdClass'],
+      parentId: json['parentId'],
+      section: json['section'],
+      stdExam: json['stdExam'],
+      stdClass: json['stdClass'],
       gender: json['gender'],
       StudentBirthDay: json['StudentBirthDay'],
       grade: json['grade'],
-      startDate:json['startDate'],
+      startDate: json['startDate'],
       eventRecords: (json['eventRecords'] as List)
           .map((event) => EventRecordModel.fromJson(event))
           .toList(),
@@ -74,6 +82,7 @@ class StudentModel {
     return 'Student(studentName: $studentName, studentNumber: $studentNumber, address: address, section: $section, gender: $gender, age: $StudentBirthDay, grade: $grade, teachers: teachers, exams: exams, startDate: $startDate,, eventRecords: $eventRecords, bus: $bus, )';
   }
 }
+
 List<StudentModel> generateRandomStudents(int count) {
   var faker = Faker();
   var random = Random();
@@ -84,7 +93,7 @@ List<StudentModel> generateRandomStudents(int count) {
       studentName: faker.person.name(),
       studentNumber: faker.randomGenerator.integer(100000).toString(),
       studentID: faker.randomGenerator.integer(100000).toString(),
-        section:"الشعبة${random.nextInt(5)}",
+      section: "الشعبة${random.nextInt(5)}",
       gender: random.nextBool() ? 'Male' : 'Female',
       parentId: faker.person.name(),
       StudentBirthDay: (random.nextInt(10) + 10).toString(),
@@ -92,7 +101,7 @@ List<StudentModel> generateRandomStudents(int count) {
       available: true,
       startDate: faker.date.dateTime(minYear: 2020, maxYear: 2023).toString(),
       eventRecords: generateRandomEvents(5),
-      bus: 'Bus ${random.nextInt(10)>5? 'Yes':'NO'}',
+      bus: 'Bus ${random.nextInt(10) > 5 ? 'Yes' : 'NO'}',
       // guardian: faker.person.name(),
     ));
   }
